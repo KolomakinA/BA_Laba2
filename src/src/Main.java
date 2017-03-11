d3facf45dc46b9985145359530450da546f0facd
Andrii Kolomakin <kolomakin@gmail.com>
2017-03-11 12:38:33 +0200

    another string

--- src/src/Main.java
+++ src/src/Main.java
@@ -2,6 +2,8 @@
  * Created by user on 11.03.2017.
  */
 public class Main  {
-
-
+    public static void main(String[] args) {
+        System.out.println("git sux");
+        System.out.println("not really");
+    }
 }
